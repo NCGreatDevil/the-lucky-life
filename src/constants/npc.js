7a27aa559a2fe135625636da4118235070eb8a38
NCGreatDevil <goodmazi@126.com>
2026-04-30 22:21:09 +0800

    更新R2头像路径为英文文件名

--- src/constants/npc.js
+++ src/constants/npc.js
@@ -1,7 +1,7 @@
 export const R2_BASE_URL = '/r2';
 
 export const NPC_AVATARS = {
-  dog_npc: `${R2_BASE_URL}?path=avatar/高冷小狗头像.png`
+  dog_npc: `${R2_BASE_URL}?path=avatar/cooldog.png`
 };
 
 export function getNpcAvatar(npcId) {
