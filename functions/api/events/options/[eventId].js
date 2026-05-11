c351c2c9488f4e7c29b835e62e561adfc76b0d78
NCGreatDevil <goodmazi@126.com>
2026-05-11 21:04:03 +0800

    fix: 修正options/[eventId].js的相对路径

--- functions/api/events/options/[eventId].js
+++ functions/api/events/options/[eventId].js
@@ -1,5 +1,5 @@
-import { corsHeaders } from '../../../../_utils.js';
-import { verifySession, getEventOptions } from '../../_utils.js';
+import { corsHeaders } from '../../../_utils.js';
+import { verifySession, getEventOptions } from '../_utils.js';
 
 export async function onRequest(context) {
     if (context.request.method === 'OPTIONS') {
