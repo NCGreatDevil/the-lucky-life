ce07ecf108dc5727b4fab1371215cf3f78caa629
NCGreatDevil <goodmazi@126.com>
2026-05-19 23:18:28 +0800

    fix: 修正祈祷图片路径，使用 /r2?path= 格式

--- src/views/Fortune.vue
+++ src/views/Fortune.vue
@@ -98,7 +98,7 @@
 
       <div v-if="isPraying" class="fixed inset-0 bg-black/80 flex items-center justify-center z-[200]">
         <div class="w-[80%] max-w-[400px] relative">
-          <img src="/r2/deity/prayer.png" alt="祈祷" class="w-full h-auto prayer-image" />
+          <img src="/r2?path=deity/prayer.png" alt="祈祷" class="w-full h-auto prayer-image" />
 
           <div class="cloud cloud-1"></div>
           <div class="cloud cloud-2"></div>
